77d3c844302bbfb1fde1ff76397fcdbdba68c14b
Clément <clement.lesaulnier@mel2.obs-mip.fr>
2017-12-11 09:46:38 +0100

    v 0.0.2

---
+++ aeris-wordpress-events-list-all/views/list/loop.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * List View Loop
+ * This file sets up the structure for the list loop
+ *
+ * Override this template in your own theme by creating a file at [your-theme]/tribe-events/list/loop.php
+ *
+ * @version 4.4
+ * @package TribeEventsCalendar
+ *
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+} ?>
+
+<?php
+global $post;
+global $more;
+$more = false;
+
+$past_events = tribe_get_events(array(
+		'eventDisplay'=>'past',
+		'posts_per_page'=>5,
+		'start_date'=>'01 '.$current_month.' 2015',
+));
+
+$new_events = tribe_get_events(array(
+		'eventDisplay'=>'upcomming',
+		'posts_per_page'=>5,
+		'start_date'=>'01 '.$current_month.' 2017',
+));
+$allEvents = array_merge($new_events,$past_events);
+$allEvents= array_unique($allEvents, SORT_REGULAR);
+
+?>
+
+<div class="tribe-events-loop">
+
+	<?php foreach ( $allEvents as $event) :  ?>
+
+		<!-- Month / Year Headers -->
+		<?php tribe_events_set_the_date_header($event); ?>
+		<!-- Event  -->
+		<?php
+		$post_parent = '';
+		if ( $event->post_parent ) {
+			$event= ' data-parent-post-id="' . absint( $event->post_parent ) . '"';
+		}
+		?>
+		<div id="post-" class="<?php tribe_events_event_classes() ?>" <?php echo $post_parent; ?>>
+			<?php
+			$event_type = tribe( 'tec.featured_events' )->is_featured( $event->ID ) ? 'featured' : 'event';
+			/**
+			 * Filters the event type used when selecting a template to render
+			 *
+			 * @param $event_type
+			 */
+			//
+			$event_type = apply_filters( 'tribe_events_list_view_event_type', $event_type );
+			//tribe_get_template_part( 'list/single', $event_type );
+			?>
+			<h2 class="tribe-events-list-event-title">
+			<a class="tribe-event-url" href="<?php echo $event->guid; ?>" title="<?php echo $event->post_title; ?>" rel="bookmark">
+			<?php echo $event->post_title; ?>	</a>
+			</h2>
+		</div>
+
+
+		<?php do_action( 'tribe_events_inside_after_loop' ); ?>
+	<?php endforeach; ?>
+
+</div><!-- .tribe-events-loop -->
